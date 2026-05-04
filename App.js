4be96c50df4f7e099ca3f1b226007706e27cd93d
Ganesh-marathi <marathiganesh112@gmail.com>
2026-05-04 10:26:52 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,32 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+import Dashboard from './pages/Dashboard';
+import AddInternship from './pages/AddInternship';
+
+const theme = createTheme({
+  palette: {
+    primary: { main: '#1976d2' },
+    secondary: { main: '#dc004e' }
+  }
+});
+
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/add" element={<AddInternship />} />
+        </Routes>
+        <ToastContainer position="top-right" />
+      </Router>
+    </ThemeProvider>
+  );
+}
+
+export default App;
